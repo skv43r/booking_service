f535ce8ed03165701f04ec57b4734a7bd237fc9d
Alexandr <skv43r@gmail.com>
2025-01-29 15:38:51 +0300

    feat(schemas): create response schema for bookings

---
+++ app/bookings/schemas.py
@@ -0,0 +1,12 @@
+from datetime import date
+from pydantic import BaseModel
+
+class SBooking(BaseModel):
+    id: int
+    room_id: int
+    user_id: int
+    date_from: date
+    date_to: date
+    price: int
+    total_coast: int
+    total_days: int
