689d5f35087fb73d3216a9045d04b74c6bc3ee77
Alexandr <skv43r@gmail.com>
2025-01-30 13:41:56 +0300

    feat(auth): add functions create_access_token and authenticate_user

--- app/users/auth.py
+++ app/users/auth.py
@@ -1,4 +1,9 @@
+from datetime import datetime, timedelta
+import jwt
 from passlib.context import CryptContext
+from pydantic import EmailStr
+from app.config import settings
+from app.users.dao import UsersDAO
 
 pwd_context = CryptContext(schemes=["bcrypt"])
 
@@ -7,3 +12,17 @@ def get_password_hash(password: str) -> str:
 
 def verify_password(plain_password, hashed_password) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=30)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, settings.ALGORITHM)
+    return encoded_jwt
+
+async def authenticate_user(email: EmailStr, password: str):
+    user = await UsersDAO.find_one_or_none(email=email)
+    if not user and not verify_password(password, user.password):
+        return None
+    return user
+    
