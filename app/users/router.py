791a50242225fb98ddf17d7d33a521cd0b5e17bf
Alexandr <skv43r@gmail.com>
2025-01-30 13:46:40 +0300

    refactor(router): create endoints: login, logout, me and edit register exception

--- app/users/router.py
+++ app/users/router.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, HTTPException
-
-from app.users.auth import get_password_hash
+from fastapi import APIRouter, Depends, Response
+from app.exceptions import UserAlreadyExistsException, IncorrectEmailOrPasswordException
+from app.users.auth import create_access_token, get_password_hash, authenticate_user
 from app.users.dao import UsersDAO
-from app.users.shemas import SUserRegister
+from app.users.dependencies import get_current_user
+from app.users.models import Users
+from app.users.shemas import SUserAuth
 
 router = APIRouter(
     prefix='/auth',
@@ -10,9 +12,26 @@ router = APIRouter(
 )
 
 @router.post("/register")
-async def register_user(user_data: SUserRegister):
+async def register_user(user_data: SUserAuth):
     existing_user = await UsersDAO.find_one_or_none(email=user_data.email)
     if existing_user:
-        raise HTTPException(status_code=400, detail="Email already exists")
+        raise UserAlreadyExistsException
     hashed_password = get_password_hash(user_data.password)
     await UsersDAO.add(email=user_data.email, hashed_password=hashed_password)
+
+@router.post("/login")
+async def login_user(response: Response, user_data: SUserAuth):
+    user = await authenticate_user(user_data.email, user_data.password)
+    if not user:
+        raise IncorrectEmailOrPasswordException
+    access_token = create_access_token({"sub": str(user.id)})
+    response.set_cookie("booking_access_token", access_token, httponly=True)
+    return access_token
+
+@router.post("/logout")
+async def logout_user(response: Response):
+    response.delete_cookie("booking_access_token")
+
+@router.get("/me")
+async def get_user_me(current_user: Users = Depends(get_current_user)):
+    return current_user
