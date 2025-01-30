2fed495eecde1a3439cafea3dca9d1fbc3e9a0c6
Alexandr <skv43r@gmail.com>
2025-01-30 13:45:09 +0300

    feat(dependencies): create functions get_token and get_current_user

---
+++ app/users/dependencies.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from fastapi import Depends, Request
+from jwt import PyJWTError
+import jwt
+from app.config import settings
+from app.exceptions import TokenExpiredException, TokenAbsentException, IncorrectTokenFormatException, UserIsNotPresentException
+from app.users.dao import UsersDAO
+
+
+def get_token(request: Request):
+    token = request.cookies.get("booking_access_token")
+    if not token:
+        raise TokenAbsentException
+    return token
+
+async def get_current_user(token: str = Depends(get_token)):
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, settings.ALGORITHM)
+    except PyJWTError:
+        raise IncorrectTokenFormatException
+    expire: str = payload.get("exp")
+    if (not expire) or (int(expire) < datetime.utcnow().timestamp()):
+        raise TokenExpiredException
+    user_id: str = payload.get("sub")
+    if not user_id:
+        raise UserIsNotPresentException
+    user = await UsersDAO.find_by_id(int(user_id))
+    if not user:
+        raise UserIsNotPresentException
+    
+    return user
