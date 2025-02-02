49f4c9d2b4779dc826f1d32ac1b93aceb1272f57
Alexandr <skv43r@gmail.com>
2025-02-02 12:27:17 +0300

    refactor(main): add cors and mount static files

--- app/main.py
+++ app/main.py
@@ -1,44 +1,37 @@
-from datetime import date
-from typing import Optional
-from fastapi import FastAPI, Query, Depends
-from pydantic import BaseModel
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.bookings.router import router as router_bookings
 from app.users.router import router as router_users
 from app.hotels.router import router as router_hotels
 from app.hotels.rooms.router import router as router_rooms
 
+from app.pages.router import router as router_pages
+from app.images.router import router as router_images
+
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 app.include_router(router_users)
 app.include_router(router_bookings)
 app.include_router(router_hotels)
-
 app.include_router(router_rooms)
 
-class SHotel(BaseModel):
-    address: str
-    name: str
-    stars: int
+app.include_router(router_pages)
+app.include_router(router_images)
 
-class HotelsSearchArgs:
-    def __init__(
-            self,
-            location: str,
-            date_from: date,
-            date_to: date,
-            has_spa: Optional[bool] = None,
-            stars: Optional[int] = Query(None, ge=1, le=5)
-    ):
-        self.location = location
-        self.date_from = date_from
-        self.date_to = date_to
-        self.has_spa = has_spa
-        self.stars = stars
+origins= [
+    "http://localhost:3000",
+]
 
-# @app.get("/hotels")
-# def get_hotels(
-#     search_args: HotelsSearchArgs = Depends()
-# ):
-#     return search_args
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers",
+                   "Access-Control-Allow-Origin",
+                   "Authorization"],
+)
