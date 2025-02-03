52c4ba35d11558ad9d1725d15ab481e78ce17db3
Sasha <skv43r@gmail.com>
2025-02-03 17:14:22 +0300

    feat(router): add celery task (send confirmation email)

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,8 +1,10 @@
 from datetime import date
 from fastapi import APIRouter, Depends
+from pydantic import TypeAdapter
 from app.bookings.dao import BookingDAO
 from app.bookings.schemas import SBooking
 from app.exceptions import RoomCannotBeBooked
+from app.tasks.tasks import send_booking_confirmation_email
 from app.users.dependencies import get_current_user
 from app.users.models import Users
 
@@ -26,6 +28,13 @@ async def add_booking(
     booking = await BookingDAO.add(user.id, room_id, date_from, date_to)
     if not booking:
         raise RoomCannotBeBooked
+    
+    booking_obj = TypeAdapter(SBooking)
+    
+    booking_dict = booking_obj.validate_python(booking).model_dump()
+    send_booking_confirmation_email.delay(booking_dict, user.email)
+
+    return booking_dict
     
 @router.delete("/{booking_id}")
 async def delete_booking(booking_id: int, user: Users = Depends(get_current_user)):
