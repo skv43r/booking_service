175bcce41588cacdb5fd13dffdae5c5ebf1fec30
Sasha <skv43r@gmail.com>
2025-02-03 17:12:08 +0300

    feat(config): add redis and smtp

--- app/config.py
+++ app/config.py
@@ -7,6 +7,15 @@ class Settings(BaseSettings):
     DB_PASS: str
     DB_NAME: str
     DB_URL: str
+
+    REDIS_HOST: str
+    REDIS_PORT: str
+
+    SMTP_PORT: str
+    SMTP_HOST: str
+    SMTP_USER: str
+    SMTP_PASS: str
+
     SECRET_KEY: str
     ALGORITHM: str
 
