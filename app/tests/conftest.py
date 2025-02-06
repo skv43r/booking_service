1cb4be50ad4d0591bfad829c478882af11215089
Alexandr <skv43r@gmail.com>
2025-02-06 17:55:32 +0300

    feat(conftest): create setup for tests

---
+++ app/tests/conftest.py
@@ -0,0 +1,71 @@
+from datetime import datetime
+import json
+import pytest_asyncio
+from httpx import AsyncClient, ASGITransport
+from sqlalchemy import insert
+from app.config import settings
+from app.database import Base, async_session_maker, engine
+from app.main import app as fastapi_app
+from app.bookings.models import Bookings
+from app.hotels.models import Hotels
+from app.hotels.rooms.models import Rooms
+from app.users.models import Users
+
+
+@pytest_asyncio.fixture(scope="session", autouse=True)
+async def prepare_database():
+    assert settings.MODE == "TEST"
+
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+    def open_mock_json(model: str):
+        with open(f"app/tests/mock_{model}.json", encoding="utf-8") as file:
+            return json.load(file)
+
+    bookings = open_mock_json("bookings")
+    hotels = open_mock_json("hotels")
+    rooms = open_mock_json("rooms")
+    users = open_mock_json("users")
+
+    for booking in bookings:
+        booking["date_from"] = datetime.strptime(booking["date_from"], "%Y-%m-%d")
+        booking["date_to"] = datetime.strptime(booking["date_to"], "%Y-%m-%d")
+
+    async with async_session_maker() as session:
+        add_hotels = insert(Hotels).values(hotels)
+        add_rooms = insert(Rooms).values(rooms)
+        add_users = insert(Users).values(users)
+        add_bookings = insert(Bookings).values(bookings)
+
+
+        await session.execute(add_hotels)
+        await session.execute(add_rooms)
+        await session.execute(add_users)
+        await session.execute(add_bookings)
+        await session.commit()
+
+@pytest_asyncio.fixture(scope="function")
+async def ac():
+    async with AsyncClient(base_url="http://testserver",
+                           transport=ASGITransport(fastapi_app)) as ac:
+        yield ac
+
+
+@pytest_asyncio.fixture(scope="session")
+async def authenticated_ac():
+    async with AsyncClient(base_url="http://testserver",
+                           transport=ASGITransport(fastapi_app)) as ac:
+        await ac.post("/auth/login", json={
+            "email": "fedor@moloko.ru",
+            "password": "test"
+        })
+        assert ac.cookies["booking_access_token"]
+        yield ac
+
+
+@pytest_asyncio.fixture(scope="function")
+async def session():
+    async with async_session_maker() as session:
+        yield session
