be642747548455d2307ffa7d9875b6998f747326
Alexandr <skv43r@gmail.com>
2025-02-06 17:54:58 +0300

    refactor(database): create setup for MODE

--- app/database.py
+++ app/database.py
@@ -1,8 +1,16 @@
+from sqlalchemy import NullPool
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 from app.config import settings
 
-engine = create_async_engine(settings.DB_URL)
+if settings.MODE == "TEST":
+    DB_URL = settings.TEST_DB_URL
+    DB_PARAMS = {"poolclass": NullPool}
+else:
+    DB_URL = settings.DB_URL
+    DB_PARAMS = {}
+
+engine = create_async_engine(DB_URL, **DB_PARAMS)
 
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
